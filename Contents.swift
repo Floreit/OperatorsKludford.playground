662584461ce9994484804081b0c22f8f57505d0c
Floreit <moontycoonn@gmail.com>
2017-03-17 09:33:47 -0500

    operators
    critical thinking

---
+++ Contents.swift
@@ -0,0 +1,26 @@
+ //: Playground - noun: a place where people can play
+ 
+ import UIKit
+ 
+ var str = "Hello, playground"
+ 
+ //eve online ore market price
+ 
+ var veldspar = 9
+ var kernite = 23
+ var scordite = 14
+ var plag = 16
+ 
+ var Invveldspar = 16 * veldspar
+ var Invkernite = 12 * kernite
+ var Invscordite = 58 * scordite
+ var Invplag = 100 * plag
+ 
+ print("you have", Invveldspar, "veldspar")
+ sleep(10)
+ print("you have", Invkernite, "kernite")
+ sleep(10)
+ print("you have", Invscordite, "scordite")
+ sleep(10)
+ print("you have", Invplag, "plag")
+ sleep(10)
